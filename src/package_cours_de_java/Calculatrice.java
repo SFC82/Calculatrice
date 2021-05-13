09cf8c8f42f169c136a927a497d612ec8387d407
Cesaire Sielatchom <syelat10@yahoo.fr>
2021-05-13 13:09:02 +0200

    Basic of the "Calculatrice" project

---
+++ src/package_cours_de_java/Calculatrice.java
@@ -0,0 +1,48 @@
+package package_cours_de_java;
+
+public class Calculatrice {
+	public Calculatrice()
+	{
+		System.out.println("Contructor called");
+	}
+    
+    public double addition()
+    {
+        double result = 0;
+        //TODO implement the addition function here ...
+        
+        return result;
+    }
+    
+    public double soustraction()
+    {
+        double result = 0;
+        //TODO implement the soustraction function here ...
+        
+        return result;
+    }
+    
+    public double multiplication()
+    {
+        double result = 0;
+        //TODO implement the multiplication function here ...
+        
+        return result;
+    }
+    
+    public double division()
+    {
+        double result = 0.0;
+        //TODO implement the division function here ...
+        
+        return result;
+    }
+    
+    public int modulo()
+    {
+        int result = 0;
+        //TODO implement the modulo function here ...
+        
+        return result;
+    }
+}
---
+++ src/package_cours_de_java/MainClass.java
@@ -0,0 +1,10 @@
+package package_cours_de_java;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO @Stephan: Implement the programm to test the class calculatrice;
+		
+	}
+
+}
