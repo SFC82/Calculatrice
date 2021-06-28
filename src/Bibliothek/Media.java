f517bfaa0685e7693b4d7d530baa25311f075207
belines <64382221+belines@users.noreply.github.com>
2021-06-28 20:12:46 +0200

    attribut position

--- src/Bibliothek/Media.java
+++ src/Bibliothek/Media.java
@@ -8,6 +8,7 @@ public class Media {
     private int  frais;
     private boolean achetable;
     private boolean inOrdnung;
+    private String position;
 
     public Media (String _id, Date _dateLoc,int _frais,boolean _achetable )
     {
@@ -16,23 +17,37 @@ public class Media {
         this.frais = _frais;
         this.achetable= _achetable;
     }
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
 
     public boolean isVerkauft()
     {
+       // if(position.equals("verkauft"))
         return achetable;
     }
     public boolean isreserviert()
     {
+       // if(position.equals("reserviert"))
         return dateLoc != null;
     }
 
     public boolean isVerfuegbar()
     {
-        return !isVerkauft() && !isreserviert();
+        if(position.equals("verfuegbar")) {
+            return !isVerkauft() && !isreserviert();
+        }
+        return false;
     }
 
     public boolean isInOrdnung()
     {
         return inOrdnung;
     }
+
+
 }
